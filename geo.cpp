19e9f6f21e8f33767565fa758006042a67dac25b
JuanCabelloVargas <jicabello@miuandes.cl>
2025-11-20 12:37:40 -0300

    feat: Add final formulas to convert easting and northing with xi and etha, calculated with kruger series limited to 4, the 4 alphas added last time

--- geo.cpp
+++ geo.cpp
@@ -55,7 +55,86 @@ UTM geo::to_UTM() {
   double n5 = n4 * n;
   double n6 = n5 * n;
 
-  double este, norte;
+  double A = (GEO_A / (1.0 + n)) *
+             (1.0 + ((1.0 / 4.0) * n2) +
+              ((1.0 / 64.0) * n4)); // 2PIA es la circunferencia del meridiano
+
+  // valores alfa, pagina 3, ecuaciones 12, en ecuaciones 35 se pueden ver mas
+  // desarrolladas (series de kruger para la transformacion)
+
+  double alpha1 = (1.0 / 2.0) * n - (2.0 / 3.0) * n2 + (5.0 / 16.0) * n3 +
+                  (41.0 / 180.0) * n4;
+  double alpha2 = (13.0 / 48.0) * n2 - (3.0 / 5.0) * n3 + (557.0 / 1440.0) * n4;
+  double alpha3 = (61.0 / 140.0) * n3 - (103.0 / 140.0) * n4;
+  double alpha4 = (49561.0 / 161280.0) * n4;
+
+  // calculo de eta y de t, donde t es la tangente de la latitud, en ecuacion 8
+  // el autor realiza el cambio para eliminar los valores de latitud y longitud
+  double t = tan(latitudRadianes);
+  double eta2 = e2 * cos(latitudRadianes) * cos(latitudRadianes) / (1.0 - e2);
+
+  // buscamos la diferencia de la longitud
+  double deltaLongitud = longitudRadianes - meridianoCenRadianes;
+
+  // calculo de sigma, se encuentra en ec9 y es parte de la eliminacion de
+  // latitud y longitud, similar a t
+  double sigma = sinh(e * tanh(e * sin(latitudRadianes) / sqrt(1.0 + t * t)));
+  // tPrima, ecuacion 7
+  double tPrima = t * sqrt(1.0 + sigma * sigma) - sigma * sqrt(1.0 + t * t);
+  double etaPrima = atan2(tPrima, cos(deltaLongitud));
+  double xiPrima =
+      atanh(sin(deltaLongitud) /
+            sqrt(tPrima * tPrima + cos(deltaLongitud) * cos(deltaLongitud)));
+
+  // ahora ya tenemos los valores para poder hacer las series de kruger
+  // simplificadas, ya que esta toma una sumatoria de 1 -> n, pero en este caso
+  // se limita a la cantidad de alphas calculados, se puede ver mas de esto en
+  // la ecuaciones 11, donde se obtiene eta y xi a partir de etaPrima y xiPrima
+  // respectivamente
+  double xi = etaPrima;
+  for (int i = 1; i < 5; i++) { // simulamos la sumatoria, pero la limitamos a 4
+    double alpha = 0;
+    if (i == 1)
+      alpha = alpha1;
+    else if (i == 2)
+      alpha = alpha2;
+    else if (i == 3)
+      alpha = alpha3;
+    else if (i == 4)
+      alpha = alpha4;
+
+    xi += alpha * sin(2 * i * etaPrima) * cosh(2 * i * xiPrima);
+  }
+
+  // de la misma manera anterior, obtenemos eta
+  double eta = xiPrima;
+  for (int i = 1; i < 5;
+       i++) { // simulamos la sumatoria, pero la limitamos a 4 debido a que son
+              // los alphas que se obtuvieron en este codigo
+    double alpha = 0;
+    if (i == 1)
+      alpha = alpha1;
+    else if (i == 2)
+      alpha = alpha2;
+    else if (i == 3)
+      alpha = alpha3;
+    else if (i == 4)
+      alpha = alpha4;
+
+    eta += alpha * cos(2 * i * etaPrima) * sinh(2 * i * xiPrima);
+  }
+
+  // como dice el autor, con esto podemos obtener los valores de este y norte
+  // (northing and easting (y,x) respecitvamente), se puede ver mas en la
+  // ecuacion 13
+
+  double este = GEO_K0 * A * eta;
+  double norte = GEO_K0 * A * xi;
+
+  // finalmente ajustamos la latitud segun el hemisferio sur
+  if (latitud < 0) {
+    norte += 10000000.0;
+  }
 
   return UTM(este, norte, zona);
 }
